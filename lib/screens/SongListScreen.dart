6497100caede091861af38583bb669004b5fbef7
HeeRyun, Jin <jins4218@gmail.com>
2021-04-07 00:04:54 +0900

    SongListScreen: Implement Sort by level && Search Function

--- lib/screens/SongListScreen.dart
+++ lib/screens/SongListScreen.dart
@@ -9,6 +9,8 @@ class SongListScreen extends StatefulWidget {
 }
 
 class _SongListScreenState extends State<SongListScreen> {
+  TextEditingController _searchString = new TextEditingController();
+
   String chartType = 'ALL';
   String levelValue = 'ALL';
 
@@ -73,25 +75,128 @@ class _SongListScreenState extends State<SongListScreen> {
       appBar: AppBar(
         title: Text("PIU Songs"),
       ),
-      body: ListView.builder(
-        itemCount: songListData.length,
-        itemBuilder: (BuildContext context, int index) {
-          var songNo = songListData[index]['songNo'];
-          var songTitleKo = songListData[index]['songTitle_ko'];
-          var songTitleEn = songListData[index]['songTitle_en'].toLowerCase();
-          var songArtistKo = songListData[index]['songArtist_ko'];
-          var songArtistEn = songListData[index]['songArtist_en'].toLowerCase();
+      body: Column(
+        children: [
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 5, 5, 5),
+                child: DropdownButton<String>(
+                  value: chartType,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.blue),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.blueAccent,
+                  ),
+                  onChanged: (newValue) {
+                    setState(
+                      () {
+                        chartType = newValue!;
+                        makeLevelList();
+                      },
+                    );
+                  },
+                  items: <String>['ALL', 'S', 'D', 'SP', 'DP', 'CO-OP']
+                      .map<DropdownMenuItem<String>>(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(5),
+                child: DropdownButton<String>(
+                  value: levelValue,
+                  icon: Icon(Icons.arrow_downward),
+                  iconSize: 24,
+                  elevation: 16,
+                  style: TextStyle(color: Colors.blue),
+                  underline: Container(
+                    height: 2,
+                    color: Colors.blueAccent,
+                  ),
+                  onChanged: (newValue) {
+                    setState(
+                      () {
+                        levelValue = newValue!;
+                        makeSongList();
+                      },
+                    );
+                  },
+                  items: levels.map(
+                    (String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    },
+                  ).toList(),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.only(left: 5, right: 10),
+                  child: SizedBox(
+                    width: 10,
+                    height: 30,
+                    child: TextField(
+                      controller: _searchString,
+                      onChanged: (text) {
+                        setState(
+                          () {
+                            SongListScreen();
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: songListData.length,
+              itemBuilder: (BuildContext context, int index) {
+                var songNo = songListData[index]['songNo'];
+                var songTitleKo = songListData[index]['songTitle_ko'];
+                var songTitleEn =
+                    songListData[index]['songTitle_en'].toLowerCase();
+                var songArtistKo = songListData[index]['songArtist_ko'];
+                var songArtistEn =
+                    songListData[index]['songArtist_en'].toLowerCase();
 
-          return ListTile(
-            dense: false,
-            leading: CircleAvatar(
-              radius: 30.0,
-              backgroundImage: AssetImage('assets/songJacket/$songNo.png'),
+                if ((songNoList[0] == 'ALL' || songNoList.contains(songNo)) &&
+                    (songTitleKo.contains(_searchString.text) ||
+                        songTitleEn
+                            .contains(_searchString.text.toLowerCase()) ||
+                        songArtistKo.contains(_searchString.text) ||
+                        songArtistEn
+                            .contains(_searchString.text.toLowerCase()))) {
+                  return ListTile(
+                    dense: false,
+                    leading: CircleAvatar(
+                      radius: 28.0,
+                      backgroundImage:
+                          AssetImage('assets/songJacket/$songNo.png'),
+                    ),
+                    title: Text(songTitleKo),
+                    subtitle: Text(songArtistKo),
+                  );
+                } else {
+                  return SizedBox(height: 0);
+                }
+              },
             ),
-            title: Text(songTitleKo),
-            subtitle: Text(songArtistKo),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
